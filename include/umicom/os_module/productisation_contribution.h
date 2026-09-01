1410f0a51911ee8ace6047aacec2606e4fe9aa9d
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-09-01 03:04:34 +0100

    feat(os): adopt shared workspace guidance

--- include/umicom/os_module/productisation_contribution.h
+++ include/umicom/os_module/productisation_contribution.h
@@ -17,6 +17,7 @@
 #define UMICOM_OS_MODULE_PRODUCTISATION_CONTRIBUTION_H
 
 #include "umicom/application/productisation/session.h"
+#include "umicom/application/productisation/workspace_guide.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,6 +29,9 @@ UmiStatus umi_os_module_productisation_snapshot(
     UmiProductApplicationAdoptionSnapshot *out_snapshot);
 UmiStatus umi_os_module_product_session_init(
     UmiProductApplicationSession *out_session);
+/* Build welcome-screen workspace choices from the canonical Framework profile. */
+UmiStatus umi_os_module_product_workspace_guide(
+    UmiProductWorkspaceGuide *out_guide);
 
 #ifdef __cplusplus
 }
